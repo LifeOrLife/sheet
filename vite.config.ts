9d810349f8aa9e21aeed2ce70309e3f1f47b5fb6
btea <2356281422@qq.com>
2022-04-16 17:21:31 +0800

    feat: 添加pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,14 @@ import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
         host: true
     },
-    plugins: [vue(), vueJsx()],
+    plugins: [vue(), vueJsx(), VitePWA()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
